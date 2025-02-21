597d774d710aadc31ec26956c4ccbac78f187b9c
Kab-35-16 <Kab-35-16@KAB-31-9->
2025-02-21 12:46:02 +0200

    2 задание

---
+++ CBookCard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_practicum
+{
+    class CBookCard
+    {
+        private string author; 
+        private string title; 
+        private string publisher; 
+        private int publicationYear; 
+        private int pagesCount; 
+        private int circulation; 
+        private short ratingFrom0To100; 
+        private string commentary;
+
+
+
+
+    }
+}
